1397e1f4b382b2c6ece45c5ff57982cbdb32a179
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-10-02 21:56:57 +0200

    Initial commit

---
+++ fold_test.go
@@ -0,0 +1,18 @@
+package ascii
+
+import "testing"
+
+func TestFold(t *testing.T) {
+	tests := []struct{ in, want string }{
+		{"æøå", "æøå"}, // 0
+		{"öl", "ol"},   // 1
+		{"Çiğdem Çiçek Şarkı Sözü", "Cigdem Cicek Sarki Sozu"},   // 2
+		{"Des mot clés À LA CHAÎNE", "Des mot cles A LA CHAINE"}, // 3
+	}
+
+	for i, test := range tests {
+		if got := Fold(test.in); got != test.want {
+			t.Errorf("%d: Fold(%q) == %q; want %q", i, test.in, got, test.want)
+		}
+	}
+}
